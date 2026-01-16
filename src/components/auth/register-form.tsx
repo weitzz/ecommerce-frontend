dde0ed47c91b0b2af390c01c0de4e4802ffa42f5
Tatiane Weitzel <weitzeltati@gmail.com>
2026-01-16 12:29:37 -0300

    ajustes

--- src/components/auth/register-form.tsx
+++ src/components/auth/register-form.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { registerAction } from '@/actions/register'
-import { useAuthStore } from '@/store/auth'
 import Link from 'next/link'
 import { redirect } from 'next/navigation'
 import React, { startTransition, useState, useTransition } from 'react'
